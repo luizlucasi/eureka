ee4b62d2e3c49dd8e98bfae664646c2599e44977
luizlucasi <luizlucasi@gmail.com>
2023-03-23 09:47:07 -0300

    Update rosavento.java

--- src/main/java/com/lucasi/eureka/rosavento.java
+++ src/main/java/com/lucasi/eureka/rosavento.java
@@ -1,36 +1,3 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Migration Documentation: Camel Routes to Spring Endpoints</title>
-	<meta charset="utf-8">
-</head>
-<body>
-	<h1>Migration Documentation: Camel Routes to Spring Endpoints</h1>
-	<p>This document describes the migration of 8 Camel routes to Spring endpoints.</p>
 
-	<h2>Route 1: [Route Name]</h2>
-	<p>Description of the route and its functionality.</p>
-	<h3>Migration Steps:</h3>
-	<ul>
-		<li>Step 1: Description of the migration step</li>
-		<li>Step 2: Description of the migration step</li>
-		<li>Step 3: Description of the migration step</li>
-		<li>Step N: Description of the migration step</li>
-	</ul>
-
-	<h2>Route 2: [Route Name]</h2>
-	<p>Description of the route and its functionality.</p>
-	<h3>Migration Steps:</h3>
-	<ul>
-		<li>Step 1: Description of the migration step</li>
-		<li>Step 2: Description of the migration step</li>
-		<li>Step 3: Description of the migration step</li>
-		<li>Step N: Description of the migration step</li>
-	</ul>
-
-	<!-- Repeat for Routes 3-8 -->
-
-	<h2>Conclusion</h2>
-	<p>Summarize the migration process and any issues encountered.</p>
-</body>
 </html>
