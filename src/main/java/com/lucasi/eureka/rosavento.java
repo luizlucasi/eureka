0e3e78c4295db5e456ba7b0b828c58e8bd14935e
luizlucasi <luizlucasi@gmail.com>
2023-03-23 09:13:16 -0300

    Update rosavento.java

--- src/main/java/com/lucasi/eureka/rosavento.java
+++ src/main/java/com/lucasi/eureka/rosavento.java
@@ -1,25 +1,36 @@
-Sure, here is an example of a four-slide presentation you can use as a basis to create your own slides in Canva:
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Migration Documentation: Camel Routes to Spring Endpoints</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Migration Documentation: Camel Routes to Spring Endpoints</h1>
+	<p>This document describes the migration of 8 Camel routes to Spring endpoints.</p>
 
-Slide 1: Introduction
+	<h2>Route 1: [Route Name]</h2>
+	<p>Description of the route and its functionality.</p>
+	<h3>Migration Steps:</h3>
+	<ul>
+		<li>Step 1: Description of the migration step</li>
+		<li>Step 2: Description of the migration step</li>
+		<li>Step 3: Description of the migration step</li>
+		<li>Step N: Description of the migration step</li>
+	</ul>
 
-Title: "Development of Data Sending via RPC"
-Description: Present the development's goal and contextualize the importance of sending data via RPC.
-[Image of a computer screen with a data sending animation]
+	<h2>Route 2: [Route Name]</h2>
+	<p>Description of the route and its functionality.</p>
+	<h3>Migration Steps:</h3>
+	<ul>
+		<li>Step 1: Description of the migration step</li>
+		<li>Step 2: Description of the migration step</li>
+		<li>Step 3: Description of the migration step</li>
+		<li>Step N: Description of the migration step</li>
+	</ul>
 
-Slide 2: Results
+	<!-- Repeat for Routes 3-8 -->
 
-Title: "Sprint Results"
-Description: Present the main functionalities and improvements implemented during the sprint, highlighting the value added to the project.
-[Image of a chart or table showing the results achieved during the sprint]
-
-Slide 3: Challenges
-
-Title: "Challenges Faced"
-Description: Identify the main challenges encountered during the sprint and describe how they were overcome.
-[Image of a team working together to overcome a challenge]
-
-Slide 4: Next Steps
-
-Title: "Next Steps"
-Description: Present the next stages of the project and explain how the team plans to approach them.
-[Image of a path or trajectory with an arrow pointing towards the future]
+	<h2>Conclusion</h2>
+	<p>Summarize the migration process and any issues encountered.</p>
+</body>
+</html>
