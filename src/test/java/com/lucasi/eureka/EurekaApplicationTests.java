3f550ce60c95af3018f2a34dadc891774b12431c
luizlucasi <luizlucasi@gmail.com>
2023-03-30 17:29:50 -0300

    Update EurekaApplicationTests.java

--- src/test/java/com/lucasi/eureka/EurekaApplicationTests.java
+++ src/test/java/com/lucasi/eureka/EurekaApplicationTests.java
@@ -1,1 +1,23 @@
 
+/**
+     * Retorna o conteúdo como uma data no formato ISO8601.
+     * @return uma data no formato ISO8601, ou null se o conteúdo não for uma data.
+     */
+    public String getDateTimeIso8601() {
+        for (Object obj : content) {
+            if (obj instanceof String) {
+                String str = (String) obj;
+                try {
+                    DateFormat df = new SimpleDateFormat("yyyyMMdd'T'HH:mm:ss");
+                    df.setTimeZone(TimeZone.getTimeZone("GMT"));
+                    Date date = df.parse(str);
+                    df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+                    df.setTimeZone(TimeZone.getDefault());
+                    return df.format(date);
+                } catch (ParseException e) {
+                    // não é uma data no formato ISO8601
+                }
+            }
+        }
+        return null;
+    }
