9b9f07b3f6cb319f459f9c95e8186c395a4a363a
luizlucasi <luizlucasi@gmail.com>
2023-03-30 17:53:38 -0300

    Update EurekaApplicationTests.java

--- src/test/java/com/lucasi/eureka/EurekaApplicationTests.java
+++ src/test/java/com/lucasi/eureka/EurekaApplicationTests.java
@@ -28,3 +28,77 @@ public class MethodResponse {
 
 }
 
+
+@XmlRootElement(name = "fault")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Fault {
+    
+    @XmlElement(name = "value")
+    private Value value;
+    
+    public Value getValue() {
+        return value;
+    }
+    
+    public void setValue(Value value) {
+        this.value = value;
+    }
+    
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Value {
+        
+        @XmlElement(name = "struct")
+        private Struct struct;
+        
+        public Struct getStruct() {
+            return struct;
+        }
+        
+        public void setStruct(Struct struct) {
+            this.struct = struct;
+        }
+    }
+    
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Struct {
+        
+        @XmlElement(name = "member")
+        private List<Member> members;
+        
+        public List<Member> getMembers() {
+            return members;
+        }
+        
+        public void setMembers(List<Member> members) {
+            this.members = members;
+        }
+    }
+    
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Member {
+        
+        @XmlElement(name = "name")
+        private String name;
+        
+        @XmlElement(name = "value")
+        private Value value;
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        public Value getValue() {
+            return value;
+        }
+        
+        public void setValue(Value value) {
+            this.value = value;
+        }
+    }
+}
+
+
