3c6e8485cd4e4e3e36428ad44dbcf814c7eceee4
luizlucasi <luizlucasi@gmail.com>
2023-03-30 18:20:09 -0300

    Update EurekaApplicationTests.java

--- src/test/java/com/lucasi/eureka/EurekaApplicationTests.java
+++ src/test/java/com/lucasi/eureka/EurekaApplicationTests.java
@@ -1,163 +1,83 @@
 
-@XmlRootElement(name = "methodResponse")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class MethodResponse {
+@XmlType(name = "DataType", propOrder = {
+    "value"
+})
+public class DataType {
 
-    @XmlElement(name = "fault")
-    private Fault fault;
+    @XmlElement(required = true)
+    protected List<ValueType> value;
 
-    @XmlElementWrapper(name = "params")
-    @XmlElement(name = "param")
-    private List<ParamType> params;
-
-    public Fault getFault() {
-        return fault;
+    public List<ValueType> getValue() {
+        if (value == null) {
+            value = new ArrayList<>();
+        }
+        return this.value;
     }
-
-    public void setFault(Fault fault) {
-        this.fault = fault;
-    }
-
-    public List<ParamType> getParams() {
-        return params;
-    }
-
-    public void setParams(List<ParamType> params) {
-        this.params = params;
-    }
-
 }
 
 
-@XmlRootElement(name = "fault")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Fault {
-    
-    @XmlElement(name = "value")
-    private Value value;
-    
-    public Value getValue() {
+@XmlType(name = "ArrayType", propOrder = {
+    "data"
+})
+public class ArrayType {
+
+    @XmlElement(required = true)
+    protected DataType data;
+
+    public DataType getData() {
+        return data;
+    }
+
+    public void setData(DataType value) {
+        this.data = value;
+    }
+}
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "MemberType", propOrder = {
+    "name",
+    "value"
+})
+public class MemberType {
+
+    @XmlElement(required = true)
+    protected String name;
+    @XmlElement(required = true)
+    protected ValueType value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public ValueType getValue() {
         return value;
     }
-    
-    public void setValue(Value value) {
+
+    public void setValue(ValueType value) {
         this.value = value;
     }
-    
-    @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Value {
-        
-        @XmlElement(name = "struct")
-        private Struct struct;
-        
-        public Struct getStruct() {
-            return struct;
-        }
-        
-        public void setStruct(Struct struct) {
-            this.struct = struct;
-        }
-    }
-    
-    @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Struct {
-        
-        @XmlElement(name = "member")
-        private List<Member> members;
-        
-        public List<Member> getMembers() {
-            return members;
-        }
-        
-        public void setMembers(List<Member> members) {
-            this.members = members;
-        }
-    }
-    
-    @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Member {
-        
-        @XmlElement(name = "name")
-        private String name;
-        
-        @XmlElement(name = "value")
-        private Value value;
-        
-        public String getName() {
-            return name;
-        }
-        
-        public void setName(String name) {
-            this.name = name;
-        }
-        
-        public Value getValue() {
-            return value;
-        }
-        
-        public void setValue(Value value) {
-            this.value = value;
-        }
-    }
 }
+
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "ValueType", propOrder = {
-    "content"
+@XmlType(name = "StructType", propOrder = {
+    "member"
 })
-public class ValueType {
+public class StructType {
 
-    @XmlMixed
-    protected List<Object> content;
+    @XmlElement(required = true)
+    protected List<MemberType> member;
 
-    @XmlAttribute(name = "type")
-    protected String type;
-
-    public List<Object> getContent() {
-        if (content == null) {
-            content = new ArrayList<>();
+    public List<MemberType> getMember() {
+        if (member == null) {
+            member = new ArrayList<>();
         }
-        return this.content;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String value) {
-        this.type = value;
-    }
-
-    public String getString() {
-        StringBuilder sb = new StringBuilder();
-        for (Object obj : getContent()) {
-            sb.append(obj.toString());
-        }
-        return sb.toString();
-    }
-
-    public boolean getBoolean() {
-        return Boolean.parseBoolean(getString());
-    }
-
-    public int getInt() {
-        return Integer.parseInt(getString());
-    }
-
-    public double getDouble() {
-        return Double.parseDouble(getString());
-    }
-
-    public byte[] getBase64() {
-        return Base64.getDecoder().decode(getString());
-    }
-
-    public Date getDateTimeIso8601() throws ParseException {
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd'T'HH:mm:ss");
-        format.setTimeZone(TimeZone.getTimeZone("GMT"));
-        return format.parse(getString());
+        return this.member;
     }
 }
-
-
-
-
