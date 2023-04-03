3bd43853ab1b1f95efcba568044a141f2d47baa8
luizlucasi <luizlucasi@gmail.com>
2023-04-03 14:43:51 -0300

    Update Params.java

--- src/test/java/com/lucasi/eureka/Params.java
+++ src/test/java/com/lucasi/eureka/Params.java
@@ -1,12 +1,33 @@
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
+@XmlAccessorType(XmlAccessType.FIELD)
+public class StructType implements ValueType {
+
+    @XmlElementWrapper(name = "struct")
+    @XmlElement(name = "member")
+    private List<MemberType> members;
+
+    public List<MemberType> getMembers() {
+        return members;
+    }
+
+    // getters and setters
+}
 
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Params {
+public class MemberType {
 
-    @XmlElement(name = "param")
-    private Param param;
+    @XmlElement(name = "name")
+    private String name;
+
+    @XmlElement(name = "value")
+    private ValueType value;
+
+    public String getName() {
+        return name;
+    }
+
+    public ValueType getValue() {
+        return value;
+    }
 
     // getters and setters
 }
