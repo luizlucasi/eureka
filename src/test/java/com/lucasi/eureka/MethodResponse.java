776f85a4c0df20fd00eae3bcc466e4430424f845
luizlucasi <luizlucasi@gmail.com>
2023-04-03 14:41:26 -0300

    Update MethodResponse.java

--- src/test/java/com/lucasi/eureka/MethodResponse.java
+++ src/test/java/com/lucasi/eureka/MethodResponse.java
@@ -6,29 +6,15 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ValueType {
 
-    @XmlElement(name = "int")
-    private Integer intValue;
-
-    @XmlElement(name = "boolean")
-    private Boolean booleanValue;
-
-    @XmlElement(name = "string")
-    private String stringValue;
-
-    @XmlElement(name = "double")
-    private Double doubleValue;
-
-    @XmlElement(name = "dateTime.iso8601")
-    @XmlJavaTypeAdapter(value = DateTimeAdapter.class)
-    private Date dateValue;
-
-    @XmlElement(name = "base64")
-    private byte[] base64Value;
-
-    @XmlElement(name = "array")
-    private Array arrayValue;
-
-    @XmlElement(name = "struct")
-    private Struct structValue;
+     @XmlElements({
+            @XmlElement(name = "int", type = Integer.class),
+            @XmlElement(name = "boolean", type = Boolean.class),
+            @XmlElement(name = "string", type = String.class),
+            @XmlElement(name = "double", type = Double.class),
+            @XmlElement(name = "dateTime.iso8601", type = Date.class),
+            @XmlElement(name = "base64", type = byte[].class),
+            @XmlElement(name = "array", type = Array.class),
+            @XmlElement(name = "struct", type = Struct.class)
+    })
 
 }
